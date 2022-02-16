8cb03b2ad32fb6748bca91613730bd41634f73f9
JOOHEE-NOAH <joohee195@gmail.com>
2022-02-16 23:06:21 +0900

    commit

---
+++ src/factory/Buyer.java
@@ -0,0 +1,14 @@
+package factory;
+
+public class Buyer extends Thread {
+	private Factory factory;
+	public Buyer(Factory factory) {
+	this.factory=factory;
+	}
+	@Override
+	public void run() {
+		for(int i=0; i<500;i++) {
+			factory.sell();
+		}
+	}
+}
